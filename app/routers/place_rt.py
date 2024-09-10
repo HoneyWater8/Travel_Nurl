ba77c45164369ac93c80b8072cd8436dd22f3778
Jungkihong <hnn07185@gmail.com>
2024-09-10 22:38:47 +0900

    SessionFeat - router 명칭 수정

--- app/routers/place_rt.py
+++ app/routers/place_rt.py
@@ -5,13 +5,17 @@ from app.cosmosdb import get_cosmos_client
 
 placeService = PlaceService(get_cosmos_client())
 
-router = APIRouter()
+router = APIRouter(prefix="/api", tags=["api"])
 
 
 @router.post("/api/tourist-images")
-async def get_random_image():
+async def get_random_image(num: int):
+    """
+    #### 랜덤으로 숫자를 가져오는 api
+    num : 가져오고자 하는 사진의 갯수 입력.
+    """
     try:
-        items = await placeService.get_random_images(5)
+        items = await placeService.get_random_images(num)
         if not items:
             raise HTTPException(status_code=404, detail="No images found")
         return items
