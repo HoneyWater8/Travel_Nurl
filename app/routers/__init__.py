ad0bb969847155faeab59329f43be61b7aa1f690
Jungkihong <hnn07185@gmail.com>
2024-09-21 13:13:22 +0900

    AI-feat: 개발을 위한 임시 후퇴

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -6,4 +6,5 @@ from .user_rt import router as user_router
 from .external_rt import router as external_router
 from .visitkorea_rt import router as visitkorea_router
 from .place_rt import router as place_router
-from .ai_rt import router as ai_router
+
+# from .ai_rt import router as ai_router
